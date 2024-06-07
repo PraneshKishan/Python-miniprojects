5013b43cffbdf1b112836226044cd17e9a843fd8
Pranesh Kishan <152611093+PraneshKishan@users.noreply.github.com>
2024-06-07 19:17:32 +0530

    Create Square_Pattern.py

---
+++ beginners/Square_Pattern.py
@@ -0,0 +1,10 @@
+# sqaure pattern
+
+#n refers to the no of rows and columns
+n=3
+
+#'i' loop is for rows and 'j' loop is for columns
+for i in range(n):
+    for j in range(n):
+        print("*", end = ' ')
+    print()
