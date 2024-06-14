d68d2652778c5010ff2d3b5a63f3f7df12300e2c
Pranesh Kishan <152611093+PraneshKishan@users.noreply.github.com>
2024-06-14 11:00:54 +0530

    Create sum_of_n_numbers.py

---
+++ beginners/sum_of_n_numbers.py
@@ -0,0 +1,9 @@
+# This program is about finding the sum of n numbers.
+#for eg: Here if the n is 5, then the program should be like 1+2+3+4+5 = 15.
+
+n = 5
+cons = 0
+
+for i in range(n+1):
+    cons = cons + i
+print(cons)
