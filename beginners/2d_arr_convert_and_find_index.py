f57ff2aa316e76a78dc046f5510bf99e80a46b56
Pranesh Kishan <152611093+PraneshKishan@users.noreply.github.com>
2024-07-05 08:29:04 +0530

    Create 2d_arr_convert_and_find_index.py

---
+++ beginners/2d_arr_convert_and_find_index.py
@@ -0,0 +1,24 @@
+#This Program is About manipulating a string into a 2D Array with user defined row and columns, with an Extra feature of finding the index position of an element from the 2d Array
+
+inp = "search_praneshonline_in_instagram
+row = int(input("Enter the row length: "))#3
+col = int(input("Enter the column length: "))#3
+
+twoD = [[]*col]*row
+
+index = 0
+
+for i in range(row):
+    lst = []
+    for j in range(col):
+        lst.append(inp[index])
+        index += 1
+        twoD[i] = lst   
+print(twoD)
+
+char = str(input("Enter the character from the array: "))
+
+for i in twoD:
+    for j in i:
+        if j == char:
+            print('x: ',twoD.index(i),'y: ',i.index(j))
